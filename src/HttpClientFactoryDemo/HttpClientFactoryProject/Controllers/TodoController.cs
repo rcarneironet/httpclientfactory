58b6e0968c00781f2d8962a9bc645e785a52a159
Ray Carneiro <ray@academiadotnet.com.br>
2020-05-30 10:00:16 -0300

    setup

---
+++ src/HttpClientFactoryDemo/HttpClientFactoryProject/Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using HttpClientFactoryProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HttpClientFactoryProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        //Injetar o servico de Todo
+        private readonly ITodoService _toDoService;
+        public TodoController(ITodoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+        [HttpGet("GetTodo/{id}")]
+        public async Task<ActionResult> GetTodo(int id)
+        {
+            return Ok(await _toDoService.GetTodo(id));
+        }
+    }
+}
